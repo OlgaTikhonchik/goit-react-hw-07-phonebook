c5b3cdd50286dc7e09f07ce7541d4fcb744d4374
OlgaTikhonchik <olga882011@gmail.com>
2023-06-14 00:58:55 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,24 +3,21 @@ import { useSelector } from 'react-redux';
 import { ContactItem } from 'components/ContactItem';
 import { ListContasts } from './ContactList.styled';
 
-import { getContacts, getFilter } from 'redux/selectors';
+import { selectContacts, selectFilter } from 'redux/selectors';
 
 export const ContactList = () => {
-  const contacts = useSelector(getContacts);
-  console.log(contacts);
-  const filter = useSelector(getFilter);
+  const contacts = useSelector(selectContacts);
 
-  // const visibleContacts = contacts.filter(({ name }) =>
-  //   name.toLowerCase().includes(filter)
-  // );
+  console.log(contacts);
+  const filter = useSelector(selectFilter);
 
   const filteredContacts = contacts?.filter(contact =>
     contact?.name?.toLowerCase().includes(filter.toLowerCase())
   );
 
-  if (!filteredContacts?.length) {
-    alert('No contacts matching your request');
-  }
+  // if (!filteredContacts?.length) {
+  //   alert('No contacts matching your request');
+  // }
 
   const store = useSelector(store => store);
   console.log(store);
@@ -31,20 +28,13 @@ export const ContactList = () => {
   return (
     <ListContasts>
       {filteredContacts.map(({ id, name, number }) => (
-        <ContactItem
-          key={id}
-          id={id}
-          name={name}
-          number={number}
-          // onDelete={onDeleteContact()}
-        />
+        <ContactItem key={id} id={id} name={name} number={number} />
       ))}
     </ListContasts>
   );
 };
 
 ContactList.propTypes = {
-  // onDelete: PropTypes.func.isRequired,
   contacs: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
